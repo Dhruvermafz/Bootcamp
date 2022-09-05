4eaa132382f3ebcc32bf9209e1ea91ab56a636a5
Dhruv verma <85825906+Dhruvermafz@users.noreply.github.com>
2022-09-05 18:15:51 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Aegon II Targeryan</h1>
+      <p>
+        was the sixth Targaryen king to sit the Iron Throne, succeeding his
+        father, Viserys I Targaryen, as Lord of the Seven Kingdoms. His ascent
+        was disputed by his older half-sister, Princess Rhaenyra Targaryen, who
+        had been their father's designated heir. Aegon and Rhaenyra fought for
+        the throne in the civil war the singers called the Dance of the Dragons,
+        in which both siblings perished.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
